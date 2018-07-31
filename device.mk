dd8327e97a492547f75b1d15cc3fe11af10d0f79
hardwareadictos <hardwareadictos@gmail.com>
2018-07-31 13:11:01 +0200

    oneplus2: build oppo-common from device tree

--- device.mk
+++ device.mk
@@ -336,4 +336,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+$(call inherit-product, device/oneplus/oneplus2/OppoCommon/common.mk)
